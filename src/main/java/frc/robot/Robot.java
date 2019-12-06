a9dd2e599724171ea2160f1a4e2601f54f334675
hannahkozar <hannahkozar@hotmail.com>
2019-12-05 19:53:52 -0500

    robot.java declare driveTrain

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,10 +19,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
+  /** 
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  */
+  public static OI m_oi;
+  public static OctoCanumDrive driveTrain = new OctoCanumDrive();
 
   /**
    * This function is run when the robot is first started up and should be
@@ -85,7 +89,9 @@ public class Robot extends TimedRobot {
    * This function is called periodically during operator control.
    */
   @Override
-  public void teleopPeriodic() {
+  public void teleopPeriodic() 
+  {
+    driveTrain.setDrives(OI.driveJoystick.getY(),OI.driveJoystick.getX());
   }
 
   /**
