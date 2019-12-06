701367e5f7bb338d0a3a0715b4d03e50af004ac5
hannahkozar <hannahkozar@hotmail.com>
2019-12-05 19:56:22 -0500

    fix error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,12 +19,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  /** 
+  
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  */
+  
   public static OI m_oi;
   public static OctoCanumDrive driveTrain = new OctoCanumDrive();
 
